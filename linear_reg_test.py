4a6b0ebc93d1a7a76cad52ea8c0e5f4db39bf4e0
bhumrapeesoonjun <bhumrapee.son@student.mahidol.edu>
2023-06-16 18:05:05 +0700

    ad

--- linear_reg_test.py
+++ linear_reg_test.py
@@ -7,17 +7,16 @@ from pandas_template import *
 from performance_categorizer import *
 from regression_model import *
 from sklearn.decomposition import PCA
-import torch
-print(torch.cuda.is_available())
+from pca_wrapper import *
 
 """ TEST SETTINGS """
 
-num_test_total = 3
-num_test_each = 3
+num_test_total = 1
+num_test_each = 1
 
 """ DATA GENERATION SETTINGS """
 
-std_settings = [10, 100, 1000]
+std_settings = [100]
 sparsity_settings = [0, 0.3, 0.5, 0.7, 0.99]
 n_settings = [1000, 10000, 100000]
 d_settings = [1000, 10000, 100000]
@@ -30,27 +29,18 @@ settings = [
 ]
 
 """ DIM REDUC SETTINGS """
-eps = [0.05, 0.1, 0.3, 0.5, 0.9]
-des = [0.05, 0.1, 0.3]
-n_components_arr = [100, 1000, 2000]
-instance_nums = [j + (i * j) for i in range(len(eps)) for j in range(len(des))]
-ep = 0.9
-de = 0.1
-instance_num = 2
-n_components = 99
-
-svd_solver = "randomized"
-model = PCA(n_components=n_components, svd_solver=svd_solver)
+eps = [0.05, 0.1, 0.5, 0.9]
+des = [0.05, 0.1]
 
 """ FUNCS """
 
 ese_jlt = [dim_reduc_function("extremely sparse JL transform", lambda x: jlt_ese(x, ep, de), {"ep": ep, "de": de}) for ep in eps for de in des]
 random_jlt = [dim_reduc_function("sparse JL transform", lambda x: jlt_r(x, ep, de), {"ep": ep, "de": de}) for ep in eps for de in des]
 n_jlt = [dim_reduc_function("JL transform", lambda x: jlt(x, ep, de), {"ep": ep, "de": de}) for ep in eps for de in des]
-pca = [dim_reduc_function("PCA", lambda x: model.fit_transform(x),  {"n_components": n_components, "svd_solver": svd_solver}) for n_components in n_components_arr]
+pca = [pca_wrapper("PCA", lambda _: _,  {})]
 blank = [dim_reduc_function("Nothing", lambda x: x, {})]
 funcs = ese_jlt + random_jlt + n_jlt + pca + blank
-num_test_funcs = [num_test_each for i in range(len(eps) * len(des) * 3)] + [3 for i in range(len(n_components_arr))] + [3]
+num_test_funcs = [num_test_each for i in range(len(eps) * len(des) * 3)] + [3] + [3]
 
 for setting in settings:
     print(f"========= Settings = {setting} ==========")
---
+++ pca_wrapper.py
@@ -0,0 +1,20 @@
+from typing import *
+
+import numpy as np
+
+from dim_reduc_function import *
+from sklearn.decomposition import PCA
+
+class pca_wrapper(dim_reduc_function):
+
+    def __init__(self, name, f, params: dict):
+        self.name: str = name
+        self.f = f
+        self.params = params
+
+    def apply(self, data: np.ndarray):
+        n_com = int(data.shape[0]//100)
+        model = PCA(n_components=n_com, svd_solver="auto")
+        self.params["n_components"] = n_com
+        self.params["svd_solver"] = "auto"
+        return model.fit_transform(data)
