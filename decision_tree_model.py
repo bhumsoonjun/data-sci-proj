fbe10b0e846de77a4929860bfa2a6222c782c3b7
Bhumrapee Soonjun <bhumsoonjun@gmail.com>
2023-07-05 14:28:35 +0700

    add result

---
+++ decision_tree_model.py
@@ -0,0 +1,39 @@
+from performance_test_model import *
+from sklearn.ensemble import RandomForestClassifier
+
+class decison_tree_model(performance_test_model):
+
+    def __init__(self):
+        self.train_data = None
+        self.train_label = None
+        self.test_data = None
+        self.test_label = None
+
+        self.n = None
+        self.d = None
+        self.model: RandomForestClassifier = None
+
+        self.reduc_train_data = None
+        self.reduc_test_data = None
+
+    def initialize(self, data: performance_test_data):
+        self.train_data = data.training_data
+        self.train_label = data.training_label
+        self.test_data = data.testing_data
+        self.test_label = data.testing_label
+        self.n, self.d = self.train_data.shape
+        self.model: RandomForestClassifier= RandomForestClassifier(criterion="gini")
+        self.compact_data = np.concatenate((self.train_data, self.test_data))
+
+
+    def apply_dim_reduc(self, dim_reduc_f) -> np.ndarray:
+        reduc = dim_reduc_f(self.compact_data)
+        self.reduc_train_data = reduc[:self.n, :]
+        self.reduc_test_data = reduc[self.n:, :]
+        return reduc
+
+    def train(self):
+        self.model.fit(self.reduc_train_data, self.train_label)
+
+    def compute_accuracy(self):
+        return self.model.score(self.reduc_test_data, self.test_label)
--- health_news_parser.py
+++ health_news_parser.py
@@ -9,10 +9,16 @@ from sklearn.model_selection import train_test_split
 
 from performance_test_data import performance_test_data
 
+subs = {"bbc", "cbc", "cnn", "everydayhealth", "fox", "gdn", "good", "kaiser", "latimes", "msn", "nbc", "npr",
+        "nytimes", "reuters", "usnews", "wsj"}
 
-def load_health_news(num_news):
-    path = 'data/health+news+in+twitter/Health-Tweets'
+def replace_in_list(x: str) -> str:
+    st = x
+    for s in subs:
+        st = st.replace(s, '')
+    return st
 
+def load_health_news(num_news, path):
     dfs = []
     lens = []
     names = []
@@ -21,7 +27,7 @@ def load_health_news(num_news):
     for filename in os.listdir(path):
         f = os.path.join(path, filename)
         if os.path.isfile(f) and cnt < k_clus:
-            dfs.append(pd.read_csv(f, sep="|", on_bad_lines='skip', encoding="latin1").iloc[:, -1])
+            dfs.append(pd.read_csv(f, sep="|", on_bad_lines='skip', encoding="latin1").iloc[:, -1].apply(lambda x: replace_in_list(x)))
             lens.append(dfs[-1].size)
             names.append(filename[:-4])
             cnt += 1
@@ -52,3 +58,35 @@ def load_health_news(num_news):
     total = total[1:, :]
     applied = np.apply_along_axis(lambda x: mean_label_mapping[x[0]], axis=1, arr=naming.reshape(-1, 1))
     return performance_test_data(training_data=train, training_label=applied, testing_data=total, testing_label=None)
+
+def load_health_news_for_decision_tree(num_news, path):
+    dfs = []
+    lens = []
+    names = []
+    k_clus = num_news
+    cnt = 0
+    for filename in sorted(os.listdir(path)):
+        f = os.path.join(path, filename)
+        if os.path.isfile(f) and cnt < k_clus:
+            dfs.append(pd.read_csv(f, sep="|", on_bad_lines='skip', encoding="latin1").iloc[:, -1].apply(lambda x: replace_in_list(x)))
+            lens.append(dfs[-1].size)
+            names.append(filename[:-4])
+            cnt += 1
+
+    aggregated_df = pd.concat(dfs)
+    vectorizer = TfidfVectorizer(stop_words=['english'])
+    X = vectorizer.fit_transform(aggregated_df).toarray()
+
+    lens = np.array([0] + lens)
+    prefix = lens.cumsum()
+    labels = np.array(functools.reduce(lambda a, b: a + b, [[names[i]] * lens[i + 1] for i in range(len(names))]))
+    means = []
+    mean_label_mapping = {}
+
+    for i in range(len(lens) - 1):
+        means.append(X[prefix[i]:prefix[i + 1], :].mean(axis=0))
+        mean_label_mapping[names[i]] = means[-1]
+
+    train, test, train_labels, test_labels = train_test_split(X, labels, test_size=k_clus*100, shuffle=True)
+
+    return performance_test_data(training_data=train, training_label=train_labels, testing_data=test, testing_label=test_labels), vectorizer.get_feature_names_out()
